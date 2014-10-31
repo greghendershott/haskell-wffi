00034f730f11d74ab6ad4545e21d1c43c065051c
Greg Hendershott <greghendershott@gmail.com>
2014-10-31 09:51:08 -0400

    Use record syntax instead of comment

--- KeyVal.hs
+++ KeyVal.hs
@@ -7,7 +7,7 @@ data KeyVal = KeyVal { key :: String -- name to use in the request
                      , val :: Value  -- value to use in the request
                      } deriving (Show)
 
-data Value = Variable (Maybe String) -- alias in request code
+data Value = Variable { alias :: (Maybe String) }
            | Constant String
            | Optional Value
            deriving (Show)
